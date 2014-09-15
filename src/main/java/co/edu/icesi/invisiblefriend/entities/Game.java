99de4eeba9b4f26457d84eaffda2e09680087a27
David Andres Manzano Herrera <damh24@gmail.com>
2014-09-15 18:17:12 -0500

    - Add some files to use JPA 2.0 an GAE Datastorage

---
+++ src/main/java/co/edu/icesi/invisiblefriend/entities/Game.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.edu.icesi.invisiblefriend.entities;
+
+import com.google.appengine.api.datastore.Key;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author David Andrés Maznzano Herrera <damanzano>
+ */
+@Entity
+public class Game {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Key key;
+    @Basic
+    private String description;
+    @Basic
+    private Date creationDate;
+    @Basic
+    private Player createdBy;
+    @Basic
+    private Date startDate;
+    @Basic
+    private Date endDate;
+    @Basic
+    private ArrayList players;
+    @Basic
+    private ArrayList friends;
+
+    public Key getKey() {
+        return key;
+    }
+
+    public void setKey(Key key) {
+        this.key = key;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Player getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Player createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public ArrayList getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(ArrayList players) {
+        this.players = players;
+    }
+
+    public ArrayList getFriends() {
+        return friends;
+    }
+
+    public void setFriends(ArrayList friends) {
+        this.friends = friends;
+    }
+    
+    
+}
---
+++ src/main/java/co/edu/icesi/invisiblefriend/entities/Player.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.edu.icesi.invisiblefriend.entities;
+
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.users.User;
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author David Andrés Maznzano Herrera <damanzano>
+ */
+@Entity
+public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Key key;
+    @Basic
+    private User googleUser;
+    @Basic
+    private String gender;
+    @Basic
+    private String location;
+
+    public Key getKey() {
+        return key;
+    }
+
+    public void setKey(Key key) {
+        this.key = key;
+    }
+
+    public User getGoogleUser() {
+        return googleUser;
+    }
+
+    public void setGoogleUser(User googleUser) {
+        this.googleUser = googleUser;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    
+    
+    
+}
